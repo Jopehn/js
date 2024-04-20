325dc6e4979a32f131cfff4c3890ee99bd8256de
Jopeh <jopehn@outlook.com.ar>
2024-04-19 23:28:57 -0300

    js

---
+++ otra_pagina.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Otra página</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="titulo">
+        <h1>Otra página</h1>
+    </div>
+    <br>
+    <div class="contenedor">
+        <a href="javascript:history.back()" class="btn">Inicio</a>
+    </div>
+</body>
+</html>
---
+++ tp_02.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP 2</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="titulo">
+        <h1>Título 1</h1>
+    </div>
+    <div class="contenedor">
+        <h2>Subtítulo</h2>
+        <p class="parrafo">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi praesentium libero voluptatem! Tempore dolorem veniam nostrum ullam corporis ab saepe, et, eius voluptatem illo error nemo eligendi dolor neque suscipit id eaque. Temporibus, nulla! Facilis distinctio ducimus earum fugiat dolorum aliquid quo, minima perspiciatis voluptatem culpa repellendus voluptate assumenda eos velit illum. Doloribus, maxime nemo quae molestias ab nulla dolores provident quam quidem nesciunt vero animi rerum corporis eaque nostrum, recusandae tempore a voluptas quaerat earum aspernatur veritatis repellendus esse modi. Pariatur atque laboriosam facere, fugit cumque beatae nulla quos sapiente nisi laborum non minima ea qui nemo, error quasi ducimus dignissimos ipsa recusandae illo sit velit quibusdam porro dolorum. Repudiandae, perspiciatis tenetur quaerat veritatis eum dignissimos tempora non sit voluptatum omnis maxime ratione quibusdam commodi nulla dolor sunt, voluptate, fugit suscipit. Beatae, maxime quam! Consequatur cum quas ad provident. Fuga, eos quasi! Atque consequatur quibusdam asperiores saepe, voluptatem nobis?</p>
+    </div>
+    <div class="titulo">
+        <h1>Título 2</h1>
+    </div>
+    <div class="contenedor">
+        <h2>Subtítulo</h2>
+        <p class="parrafo">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores tenetur quo cum placeat reprehenderit non enim necessitatibus ullam. Voluptate reiciendis accusamus ducimus tempora dolorem dolore quidem ipsa hic velit quisquam?</p>
+        <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, dignissimos?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, dignissimos?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, dignissimos?</li>
+        </ul>
+        <ol>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+        </ol>
+        <div class="img">
+            <img src="riva_del_garda.jpg" alt="" title="Riva del Garda" width="100%">
+        </div>
+        <a href="otra_pagina.php" class="btn">Otra página</a>
+    </div>
+</body>
+</html>
