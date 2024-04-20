bedc1035b51a080a348c57642a9aa4324cd8ee68
Jopeh <jopehn@outlook.com.ar>
2024-04-19 23:44:52 -0300

    modified:   index.htm 	new file:   tp_01.php

---
+++ tp_01.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <link href='https://res.webserver.ar/img/gm_ico.png' rel='shortcut icon' type='image/png'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP 01</title>
+</head>
+
+<body>
+    <h1>Título Uno</h1>
+    <h2>Subtítulo</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius minima quae voluptate eaque illo, vitae
+        velit magnam possimus dignissimos ipsum sapiente excepturi dolorem. Maiores commodi laudantium ipsum quia at
+        unde nam aliquid aliquam, facilis cum, culpa officia similique ducimus consequatur, quis architecto! Eaque quas
+        culpa natus? Non velit illum nisi rerum tempora facere, totam quis sed hic. Unde quo vero aspernatur, nisi
+        laboriosam voluptatem sequi eos obcaecati voluptas ad culpa nesciunt iste corrupti perspiciatis eveniet,
+        laudantium nobis. Veritatis deserunt neque perferendis atque rem odio non molestias iste vero voluptatem
+        consectetur consequuntur recusandae debitis voluptatum quos labore quibusdam voluptas eos accusantium, facilis
+        corporis et aliquam. Exercitationem minus excepturi labore commodi voluptates eum, dignissimos neque rerum
+        natus, minima explicabo praesentium eligendi! In tempora numquam blanditiis debitis maxime quas pariatur
+        distinctio est. Sint consequuntur quam doloribus aliquam mollitia, harum sed illum molestiae saepe voluptates
+        aliquid et dolorem veniam non necessitatibus ex velit quae dolore expedita nulla nostrum consectetur dolorum?
+        Tenetur a aperiam dignissimos fugiat quis minus ut repellat asperiores magnam nemo, atque quam optio tempora
+        eveniet pariatur. Vero vitae quasi voluptas quas, aliquam ad debitis unde ab ipsa eligendi nesciunt repellendus
+        harum explicabo ipsum officiis, quibusdam iure quos quisquam culpa voluptatibus et.
+    </p>
+
+    <hr>
+    <h1>Título Dos</h1>
+    <h2>Subtítulo</h2>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente animi minus asperiores voluptatem quidem qui
+        perferendis iusto nobis consectetur dolorem. Consectetur ea, itaque natus beatae necessitatibus sequi quis ipsa
+        amet excepturi nihil enim provident asperiores officiis nesciunt eveniet velit quia delectus laborum quos
+        facilis fuga? Ipsum hic, asperiores culpa repudiandae maiores eos architecto natus cum exercitationem enim nam
+        totam provident delectus! Fugiat cumque blanditiis ipsa mollitia dignissimos commodi dolor tempore repellat
+        architecto aut soluta nobis, modi ab quae laboriosam asperiores necessitatibus laborum ullam non iusto maiores
+        saepe! Nulla, nisi. Architecto, quia! Mollitia, quaerat fugit cumque vero porro ducimus laudantium consequuntur!
+    </p>
+    <ul>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+    </ul>
+
+    <ol>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+    </ol>
+    <hr>
+    <img width="50%" src="riva_del_garda.jpg" alt="Riva del Garda" title="Riva del Garda">
+    <br>
+    <a href="otra_pagina.php">OTRA PAGINA</a>
+</body>
+
+</html>
